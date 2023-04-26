249677aa30e1060a3a3fe6ddd7f64c9d5dfe251c
Никита <129552667+rar-exe@users.noreply.github.com>
2023-04-27 01:00:55 +0300

    Add files via upload

---
+++ overviewerConfig.js
@@ -0,0 +1,74 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "world"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1656994710",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Overviewer Render (smooth_lighting)",
+            "zoomLevels": 11,
+            "defaultZoom": 1,
+            "maxZoom": 11,
+            "path": "world-smooth_lighting",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "world",
+            "last_rendertime": 0,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                253,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                0,
+                253,
+                0
+            ],
+            "north_direction": 0,
+            "render_in_progress": true,
+            "forcerendertime": 1656994692
+        }
+    ]
+};
